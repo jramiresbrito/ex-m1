80fc52ba6f91a4e42440d6a7588bdb442c1713f9
Joao Victor Ramires Guimaraes Brito <joaoramires.engsoft@gmail.com>
2020-07-12 11:16:16 -0300

    Finish the app

--- src/index.js
+++ src/index.js
@@ -20,7 +20,7 @@ window.addEventListener('load', () => {
   tabCountries = document.querySelector('#tabCountries');
   tabFavorites = document.querySelector('#tabFavorites');
 
-  countCountries = document.querySelector('#countContries');
+  countCountries = document.querySelector('#countCountries');
   countFavorites = document.querySelector('#countFavorites');
 
   globalPopulation = document.querySelector('#globalPopulation');
@@ -31,6 +31,134 @@ window.addEventListener('load', () => {
   fetchCountries();
 });
 
-const fetchCountries = () => {
-  console.log('fetching...');
+const fetchCountries = async () => {
+  const res = await fetch('https://restcountries.eu/rest/v2/all');
+  allCountries = await res.json();
+  allCountries = allCountries.map((country) => {
+    const { numericCode, translations, population, flag } = country;
+    return {
+      id: numericCode,
+      name: translations.pt,
+      population,
+      flag,
+    };
+  });
+
+  render();
+};
+
+const render = () => {
+  renderCountryList();
+  renderFavorites();
+  renderSummary();
+  renderCountryButtons();
+};
+
+const renderCountryList = () => {
+  let countriesHTML = '<div>';
+
+  allCountries.forEach((country) => {
+    const { name, flag, id, population } = country;
+
+    const countryHTML = `
+      <div class='country'>
+        <div>
+          <a id="${id}" class="waves-effect waves-light btn">+</a>
+        </div>
+        <div>
+          <img src="${flag}" alt="${name}" />
+        </div>
+        <div>
+          <ul>
+            <li>${name}</li>
+            <li>${population}</li>
+          </ul>
+        </div>
+      </div>
+    `;
+
+    countriesHTML += countryHTML;
+  });
+
+  countriesHTML += '</div>';
+  tabCountries.innerHTML = countriesHTML;
+};
+
+const renderFavorites = () => {
+  let favoritesHTML = '<div>';
+
+  favCountries.forEach((country) => {
+    const { name, flag, id, population } = country;
+
+    const favCountryHTML = `
+      <div class='country'>
+        <div>
+          <a id="${id}" class="waves-effect waves-light btn red darken-4">+</a>
+        </div>
+        <div>
+          <img src="${flag}" alt="${name}" />
+        </div>
+        <div>
+          <ul>
+            <li>${name}</li>
+            <li>${population}</li>
+          </ul>
+        </div>
+      </div>
+    `;
+
+    favoritesHTML += favCountryHTML;
+  });
+
+  favoritesHTML += '</div>';
+  tabFavorites.innerHTML = favoritesHTML;
+};
+
+const renderSummary = () => {
+  countCountries.textContent = allCountries.length;
+  countFavorites.textContent = favCountries.length;
+
+  const totalPop = allCountries.reduce((acc, curr) => acc + curr.population, 0);
+
+  globalPopulation.textContent = totalPop;
+
+  const favPop = favCountries.reduce((acc, curr) => acc + curr.population, 0);
+  favoritesPopulation.textContent = favPop;
+};
+
+const renderCountryButtons = () => {
+  const allCountriesButtons = Array.from(tabCountries.querySelectorAll('.btn'));
+  const favCountriesButtons = Array.from(tabFavorites.querySelectorAll('.btn'));
+
+  allCountriesButtons.forEach((btn) => {
+    btn.addEventListener('click', () => addToFavorites(btn.id));
+  });
+
+  favCountriesButtons.forEach((btn) => {
+    btn.addEventListener('click', () => removeFromFavorites(btn.id));
+  });
+};
+
+const addToFavorites = (id) => {
+  const countryToAdd = allCountries.find((btn) => btn.id === id);
+
+  favCountries = [...favCountries, countryToAdd];
+
+  favCountries.sort((a, b) => a.name.localeCompare(b.name));
+
+  allCountries = allCountries.filter((country) => country.id !== id);
+
+  render();
+};
+
+const removeFromFavorites = (id) => {
+  const countryToRemove = favCountries.find((btn) => btn.id === id);
+
+  allCountries = [...allCountries, countryToRemove];
+
+  allCountries.sort((a, b) => a.name.localeCompare(b.name));
+
+  favCountries = favCountries.filter((country) => country.id !== id);
+
+  render();
 };
